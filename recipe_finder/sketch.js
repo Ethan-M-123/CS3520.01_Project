c2fd46202caa8923fe9360e7b1807cd3b34f6b29
Nicholas Piazza <59896247+airpiazza@users.noreply.github.com>
2021-04-15 18:47:08 -0700

    added clear button
    When you press the button clear, it clears list of ingredients saved and the recipe link.

--- recipe_finder/sketch.js
+++ recipe_finder/sketch.js
@@ -18,6 +18,12 @@ let recipeList = {
 };
 // link to recipe to be initialized
 let recipeLink;
+// div element to be initialized
+let div;
+// clear button to be intitialized
+let clear;
+// ingredient p array
+let ingredientPArray = [];
 
 // function for what happens once project is run
 function setup() {
@@ -39,6 +45,13 @@ function setup() {
   // search button
   search = createButton('search');
   search.mousePressed(findRecipe);
+
+  // clear button
+  clear = createButton('clear');
+  clear.mousePressed(clearIngredients);
+
+  // create a div for where recipes are to be displayed
+  div = createDiv();
 }
 
 // called when model is ready
@@ -64,22 +77,33 @@ function gotIngredient(error, ingredients){
   classifyIngredient();
 }
 
+// gets called when user clicks save
 function displayIngredient(){
+  // push ingredient onto ingredientsSaved array
   ingredientsSaved.push(ingredientName);
-  createP(ingredientName);
+  // create a p element with the name of the ingredient
+  let ingredientP = createP(ingredientName);
+  // make the p element a child of the div
+  div.child(ingredientP);
+  // push p element of ingredient onto ingredientPArray
+  ingredientPArray.push(ingredientP);
 }
 
+// start tau prolog sesson
 var session = pl.create();
 
+// gets called when user clicks search
 function findRecipe(){
-  
+  // load recipes.pl file
   session.consult("/recipes.pl", {
     success:function(){
+      // query recipes.pl with list of ingredients
       session.query("recipe("+ingredientsSaved+", X).", {
         success: function(goal){
           session.answer({
             success: function(answer){
               console.log(session.format_answer(answer));
+              // call display recipe with result of query
               displayRecipe(session.format_answer(answer));
             }
           });
@@ -90,7 +114,22 @@ function findRecipe(){
   });
 }
 
+// gets called after succesful query of recipes.pl
 function displayRecipe(recipe){
+  // creates a link to recipe found in recipeList
   recipeLink = createA(recipeList[recipe], 'recipe', '_blank');
+  // center the link
   recipeLink.center('horizontal');
+}
+
+// called when user clicks clear
+function clearIngredients(){
+  // make ingredientsSaved array empty
+  ingredientsSaved = [];
+  // delete all the ingredient p elements
+  for(let i = 0; i < ingredientPArray.length; i++){
+    ingredientPArray[i].remove();
+  }
+  // remove recipe link if present
+  recipeLink.remove();
 }
