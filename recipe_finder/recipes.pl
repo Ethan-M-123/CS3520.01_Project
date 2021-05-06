02f4e9455b1ec0bdbd5e70de73d7e0ee0206c2e4
unusualpanda69 <71245510+unusualpanda69@users.noreply.github.com>
2021-05-06 01:49:48 -0700

    Update recipes.pl
    bloop

--- recipe_finder/recipes.pl
+++ recipe_finder/recipes.pl
@@ -5,3 +5,58 @@ recipe(cheese, tortilla, quesadilla).
 recipe(bread, cheese, egg, eggAndCheeseToast).
 recipe(bread, egg, eggInHole).
 
+
+
+
+
+%Breakfast
+recipe(egg, cheese, sausage, Sausage Omelette).
+recipe(egg, cheese, bacon, Bacon Omelette).
+recipe(egg, cheese, bacon, sausage, Combo Omelette).
+recipe(egg, cheese, Cheese Omelette).
+recipe(egg, bacon, sausage, pancake batter, milk, butter, Super Breakfast).
+recipe(sausage, egg, milk, mustard, bread, shredded cheese, Sausage breakfast Cassarole).
+recipe(flour, sugar, baking powder, egg, milk, butter, strawberry, Strawberry Waffles).
+recipe(bacon, onion, potato, egg, shredded cheese, Sheepherders Breakfast).
+recipe(sausage, egg tortilla, potato, Sausage Breakfast Burrito).
+recipe(bacon, egg tortilla, potato, Bacon Breakfast Burrito).
+recipe(bacon, sausage, egg tortilla, potato, Combo Breakfast Burrito).
+recipe(peanut butter, oatmeal, honey, Peatun Butter Oatmeal).
+recipe(potato, shredded cheese, sour cream, shredded cheese, Cheesy Hash Brown Bake).
+recipe(strawberry, banana, raspberry, blueberry, spinach, Berry Smoothie).
+recipe(butter, sweet potato, garlic, spinach, egg, Sweet Potato and Egg Skillet).
+recipe(potato, bacon, shredded cheese, egg, milk, Hash Brown Egg Bake).
+
+%lunch
+recipe(bread, lunch meat, boring sandwich).
+recipe(bread, lunch meat, cheese, tomato, avocado, fancy sandwich).
+recipe(lettuce, blueberry, almonds, balsamic vinegar, cheese, mustard, oil, Summer Blueberry Almond Salad).
+recipe(potato, sour cream, shredded cheese, Microwave Baked Potato).
+recipe(tortilla, shredded cheese, Cheese Quesadilla).
+recipe(bean, rice, tortilla, Bean and Cheese Burrito).
+recipe(tortilla, tomato, avocado, lunch meat, Wrap).
+recipe(tuna, mayonnaise, Tuna Salad).
+
+
+
+%Dinner
+recipe(ground beef, lasagna noodle, tomato, shredded cheese, Lasagna).
+recipe(noodle, pasta sauce, Spaghetti).
+recipe(pepperoni, bread, shredded cheese, pasta sauce, Pepperoni Pizza).
+recipe(tortilla, chicken, tomato, onion, Chicken Tacos).
+recipe(ramen, bacon, egg, Bacon and Egg Ramen).
+recipe(brocolli, noodle, shredded cheese, milk, Brocolli Mac&Cheese).
+recipe(chicken, tomato, pesto, white cheese, Pesto Chicken Bake).
+recipe(salmon, asparagus, lemon, garlic, Lemon Garlic Salmon).
+recipe(tuna, avocado, tortilla, spinach, carrot, Tuna Wrap).
+recipe(ground beef, onion, tomato, brocolli, sweet potato, ketchup, Meatloaf Dinner).
+recipe(green bean, brocolli, baby corn, mushroom, bell pepper, cucumber, soy sauce, Vegetable Stir Fry).
+
+%dessert
+recipe(cookie, ice cream, ice cream sandwich).
+recipe(milk, strawberry, strawberry milkshake).
+recipe(milk, chocolate, chocolate milkshake).
+recipe(root beer, ice cream, root beer float).
+recipe(peanut butter, oatmeal, honey, Peanut Butter Oatmeal Bars).
+recipe(marshmallow, chocolate, smores).
+recipe(pretzels, chocolate, Chocolate Covered Pretzels).
