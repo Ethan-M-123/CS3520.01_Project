bda632a88b7cb89a03cd5cfffb05b1dc63da5adb
Ethan-M-123 <79026999+Ethan-M-123@users.noreply.github.com>
2021-05-12 14:35:15 -0700

    Update recipes.pl
    removed capital letters

--- recipe_finder/recipes.pl
+++ recipe_finder/recipes.pl
@@ -1,47 +1,47 @@
 %Breakfast
-recipe(egg, cheese, sausage, Sausage_Omelette).
-recipe(egg, cheese, bacon, Bacon_Omelette).
-recipe(egg, cheese, bacon, sausage, Lumberjack_Omelette).
-recipe(egg, cheese, Cheese_Omelette).
-recipe(egg, bacon, sausage, potato, bread, cheese, butter, milk, Cheesy_Bacon-Sausage-Egg-Hash_Brown_Skillet).
-recipe(sausage, egg, milk, mustard, bread, shredded_cheese, Sausage_breakfast_Cassarole).
-recipe(bacon, onion, potato, egg, shredded_cheese, Sheepherders_Breakfast).
-recipe(sausage, egg, tortilla, potato, Sausage_Breakfast_Burrito).
-recipe(bacon, egg, tortilla, potato, Bacon_Breakfast_Burrito).
-recipe(bacon, sausage, egg, tortilla, potato, Combo_Breakfast_Burrito).
-recipe(potato, shredded_cheese, milk, shredded_cheese, Cheesy_Hash_Brown_Bake).
-recipe(strawberry, banana, blueberry, Berry_Smoothie).
-recipe(butter, sweet_potato, garlic, egg, Sweet_Potato_and_Egg_Skillet).
-recipe(potato, bacon, shredded_cheese, egg, milk, Hash_Brown_Egg_Bake).
+recipe(egg, cheese, sausage, sausage_omelette).
+recipe(egg, cheese, bacon, bacon_omelette).
+recipe(egg, cheese, bacon, sausage, lumberjack_omelette).
+recipe(egg, cheese, cheese_omelette).
+recipe(egg, bacon, sausage, potato, bread, cheese, butter, milk, cheesy_bacon_sausage_egg_hash_brown_skillet).
+recipe(sausage, egg, milk, mustard, bread, shredded_cheese, sausage_breakfast_cassarole).
+recipe(bacon, onion, potato, egg, shredded_cheese, sheepherders_breakfast).
+recipe(sausage, egg, tortilla, potato, sausage_breakfast_burrito).
+recipe(bacon, egg, tortilla, potato, bacon_breakfast_burrito).
+recipe(bacon, sausage, egg, tortilla, potato, combo_breakfast_burrito).
+recipe(potato, shredded_cheese, milk, shredded_cheese, cheesy_hash_brown_bake).
+recipe(strawberry, banana, blueberry, berry_smoothie).
+recipe(butter, sweet_potato, garlic, egg, sweet_potato_and_egg_skillet).
+recipe(potato, bacon, shredded_cheese, egg, milk, hash_brown_egg_bake).
 
 %lunch
-recipe(bread, cheese, Grilled_cheese).
+recipe(bread, cheese, grilled_cheese).
 recipe(bread, lunch_meat, cheese, tomato, avocado, fancy_sandwich).
-recipe(lettuce, blueberry, almonds, balsamic_vinegar, cheese, mustard, Summer_Blueberry_Almond_Salad).
-recipe(potato, milk, shredded_cheese, Baked_Potato).
-recipe(tortilla, shredded_cheese, Cheese_Quesadilla).
-recipe(bean, tortilla, Bean_and_Cheese_Burrito).
-recipe(tortilla, tomato, avocado, lunch_meat, Wrap).
-recipe(lunch_meat, lettuce, shredded_cheese, Chef_Salad).
+recipe(lettuce, blueberry, almonds, balsamic_vinegar, cheese, mustard, summer_blueberry_almond_salad).
+recipe(potato, milk, shredded_cheese, baked_potato).
+recipe(tortilla, shredded_cheese, cheese_quesadilla).
+recipe(bean, tortilla, bean_and_cheese_burrito).
+recipe(tortilla, tomato, avocado, lunch_meat, wrap).
+recipe(lunch_meat, lettuce, shredded_cheese, chef_salad).
 
 
 
 %Dinner
-recipe(ground_beef, lasagna_noodle, tomato, shredded_cheese, Lasagna).
-recipe(noodle, tomatoes, Spaghetti).
-recipe(bread, shredded_cheese, tomatoes, Pizza).
-recipe(tortilla, chicken, tomato, onion, Chicken_Tacos).
-recipe(noodle, bacon, egg, Bacon_and_Egg_Pasta).
-recipe(brocolli, noodle, shredded_cheese, milk, Brocolli_Mac&Cheese).
-recipe(chicken, tomato, shredded_cheese, Chicken_Bake).
-recipe(chicken, asparagus, lemon, garlic, Lemon_Garlic_Chicken).
-recipe(lunch_meat, avocado, tortilla, lettuce, carrot, Hefty_Wrap).
-recipe(ground_beef, onion, tomato, brocolli, sweet_potato, ketchup, Meatloaf).
-recipe(green_bean, brocolli, baby_corn, mushroom, bell_pepper, cucumber, soy_sauce, Vegetable_Stir_Fry).
+recipe(ground_beef, lasagna_noodle, tomato, shredded_cheese, lasagna).
+recipe(noodle, tomatoes, spaghetti).
+recipe(bread, shredded_cheese, tomatoes, pizza).
+recipe(tortilla, chicken, tomato, onion, chicken_tacos).
+recipe(noodle, bacon, egg, bacon_and_egg_pasta).
+recipe(brocolli, noodle, shredded_cheese, milk, brocolli_mac&cheese).
+recipe(chicken, tomato, shredded_cheese, chicken_bake).
+recipe(chicken, asparagus, lemon, garlic, lemon_garlic_chicken).
+recipe(lunch_meat, avocado, tortilla, lettuce, carrot, hefty_wrap).
+recipe(ground_beef, onion, tomato, brocolli, sweet_potato, ketchup, meatloaf).
+recipe(green_bean, brocolli, baby_corn, mushroom, bell_pepper, cucumber, soy_sauce, vegetable_stir_fry).
 
 %dessert
 recipe(cookie, ice_cream, ice_cream_sandwich).
 recipe(milk, strawberry, strawberry_milkshake).
 recipe(milk, chocolate, chocolate_milkshake).
 recipe(marshmallow, chocolate, smores).
-recipe(apple, chocolate, Chocolate_Covered_Apple).
+recipe(apple, chocolate, chocolate_covered_apple).
