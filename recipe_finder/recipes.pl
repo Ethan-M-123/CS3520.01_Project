88ab4fe216b11b676952318c53b678ff7bedb8fd
Ethan-M-123 <79026999+Ethan-M-123@users.noreply.github.com>
2021-05-12 14:28:10 -0700

    Update recipes.pl
    changed spaces to underscores for the recipe ingredients

--- recipe_finder/recipes.pl
+++ recipe_finder/recipes.pl
@@ -4,43 +4,43 @@ recipe(egg, cheese, bacon, Bacon_Omelette).
 recipe(egg, cheese, bacon, sausage, Lumberjack_Omelette).
 recipe(egg, cheese, Cheese_Omelette).
 recipe(egg, bacon, sausage, potato, bread, cheese, butter, milk, Cheesy_Bacon-Sausage-Egg-Hash_Brown_Skillet).
-recipe(sausage, egg, milk, mustard, bread, shredded cheese, Sausage_breakfast_Cassarole).
-recipe(bacon, onion, potato, egg, shredded cheese, Sheepherders_Breakfast).
+recipe(sausage, egg, milk, mustard, bread, shredded_cheese, Sausage_breakfast_Cassarole).
+recipe(bacon, onion, potato, egg, shredded_cheese, Sheepherders_Breakfast).
 recipe(sausage, egg, tortilla, potato, Sausage_Breakfast_Burrito).
-recipe(bacon, egg tortilla, potato, Bacon_Breakfast_Burrito).
-recipe(bacon, sausage, egg tortilla, potato, Combo_Breakfast_Burrito).
-recipe(potato, shredded cheese, milk, shredded cheese, Cheesy_Hash_Brown_Bake).
+recipe(bacon, egg, tortilla, potato, Bacon_Breakfast_Burrito).
+recipe(bacon, sausage, egg, tortilla, potato, Combo_Breakfast_Burrito).
+recipe(potato, shredded_cheese, milk, shredded_cheese, Cheesy_Hash_Brown_Bake).
 recipe(strawberry, banana, blueberry, Berry_Smoothie).
-recipe(butter, sweet potato, garlic, egg, Sweet_Potato_and_Egg_Skillet).
-recipe(potato, bacon, shredded cheese, egg, milk, Hash_Brown_Egg_Bake).
+recipe(butter, sweet_potato, garlic, egg, Sweet_Potato_and_Egg_Skillet).
+recipe(potato, bacon, shredded_cheese, egg, milk, Hash_Brown_Egg_Bake).
 
 %lunch
 recipe(bread, cheese, Grilled_cheese).
-recipe(bread, lunch meat, cheese, tomato, avocado, fancy_sandwich).
-recipe(lettuce, blueberry, almonds, balsamic vinegar, cheese, mustard, Summer_Blueberry_Almond_Salad).
-recipe(potato, milk, shredded cheese, Baked_Potato).
-recipe(tortilla, shredded cheese, Cheese_Quesadilla).
-recipe(bean, tortilla, Bean and Cheese_Burrito).
-recipe(tortilla, tomato, avocado, lunch meat, Wrap).
-recipe(lunch meat, lettuce, shredded cheese, Chef_Salad).
+recipe(bread, lunch_meat, cheese, tomato, avocado, fancy_sandwich).
+recipe(lettuce, blueberry, almonds, balsamic_vinegar, cheese, mustard, Summer_Blueberry_Almond_Salad).
+recipe(potato, milk, shredded_cheese, Baked_Potato).
+recipe(tortilla, shredded_cheese, Cheese_Quesadilla).
+recipe(bean, tortilla, Bean_and_Cheese_Burrito).
+recipe(tortilla, tomato, avocado, lunch_meat, Wrap).
+recipe(lunch_meat, lettuce, shredded_cheese, Chef_Salad).
 
 
 
 %Dinner
-recipe(ground beef, lasagna noodle, tomato, shredded cheese, Lasagna).
+recipe(ground_beef, lasagna_noodle, tomato, shredded_cheese, Lasagna).
 recipe(noodle, tomatoes, Spaghetti).
-recipe(bread, shredded cheese, tomatoes, Pizza).
+recipe(bread, shredded_cheese, tomatoes, Pizza).
 recipe(tortilla, chicken, tomato, onion, Chicken_Tacos).
 recipe(noodle, bacon, egg, Bacon_and_Egg_Pasta).
-recipe(brocolli, noodle, shredded cheese, milk, Brocolli_Mac&Cheese).
-recipe(chicken, tomato, shredded cheese, Chicken_Bake).
+recipe(brocolli, noodle, shredded_cheese, milk, Brocolli_Mac&Cheese).
+recipe(chicken, tomato, shredded_cheese, Chicken_Bake).
 recipe(chicken, asparagus, lemon, garlic, Lemon_Garlic_Chicken).
-recipe(lunch meat, avocado, tortilla, lettuce, carrot, Hefty_Wrap).
-recipe(ground beef, onion, tomato, brocolli, sweet potato, ketchup, Meatloaf).
-recipe(green bean, brocolli, baby corn, mushroom, bell pepper, cucumber, soy sauce, Vegetable_Stir_Fry).
+recipe(lunch_meat, avocado, tortilla, lettuce, carrot, Hefty_Wrap).
+recipe(ground_beef, onion, tomato, brocolli, sweet_potato, ketchup, Meatloaf).
+recipe(green_bean, brocolli, baby_corn, mushroom, bell_pepper, cucumber, soy_sauce, Vegetable_Stir_Fry).
 
 %dessert
-recipe(cookie, ice cream, ice_cream_sandwich).
+recipe(cookie, ice_cream, ice_cream_sandwich).
 recipe(milk, strawberry, strawberry_milkshake).
 recipe(milk, chocolate, chocolate_milkshake).
 recipe(marshmallow, chocolate, smores).
