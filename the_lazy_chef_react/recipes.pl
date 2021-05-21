c3905d3a7af89bd4c6013ca2ec1ae67faf555910
Nicholas Piazza <59896247+airpiazza@users.noreply.github.com>
2021-05-20 18:18:36 -0700

    committed this to wrong file

--- the_lazy_chef_react/recipes.pl
+++
@@ -1,8 +0,0 @@
-recipe(lemon, zest).
-recipe(bread, nutella, breadAndNutella).
-recipe(avocado, bread, egg, avocadoToast).
-recipe(cheese, tortilla, quesadilla).
-recipe(bread, cheese, egg, eggAndCheeseToast).
-recipe(bread, egg, eggInHole).
-
-recipe(hello, no, nothing).
