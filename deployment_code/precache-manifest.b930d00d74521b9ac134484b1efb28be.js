29eb615adc89cc0a6096e7abdaf74908390ae1e7
Nicholas Piazza <59896247+airpiazza@users.noreply.github.com>
2021-05-21 00:27:09 -0700

    adding deployment code

---
+++ deployment_code/precache-manifest.b930d00d74521b9ac134484b1efb28be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891b8cbb7cfd2a1bcf1e418eeda6f7f5",
+    "url": "/the-lazy-chef-deploy-this/index.html"
+  },
+  {
+    "revision": "13dc39b83786fea61b1f",
+    "url": "/the-lazy-chef-deploy-this/static/css/main.edf53e03.chunk.css"
+  },
+  {
+    "revision": "409e7cbd643a1facc6c2",
+    "url": "/the-lazy-chef-deploy-this/static/js/2.457cb57b.chunk.js"
+  },
+  {
+    "revision": "140009cd4134f44b73316f51861b7b3a",
+    "url": "/the-lazy-chef-deploy-this/static/js/2.457cb57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13dc39b83786fea61b1f",
+    "url": "/the-lazy-chef-deploy-this/static/js/main.7d066789.chunk.js"
+  },
+  {
+    "revision": "501aeaae174bd8081772",
+    "url": "/the-lazy-chef-deploy-this/static/js/runtime-main.3020b152.js"
+  }
+]);
